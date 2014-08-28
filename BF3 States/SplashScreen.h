e4c70cfb102f899b8a134384f014859b149d294f
Palaniya World <palaniya@Palaniyas-iMac.local>
2014-08-28 18:55:44 +0530

    Initial Start for application.

---
+++ BF3 States/SplashScreen.h
@@ -0,0 +1,13 @@
+//
+//  SplashScreen.h
+//  BF3 States
+//
+//  Created by Palaniya World on 28/08/14.
+//  Copyright (c) 2014 Palaniya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SplashScreen : UIViewController
+
+@end
---
+++ BF3 States/SplashScreen.m
@@ -0,0 +1,49 @@
+//
+//  SplashScreen.m
+//  BF3 States
+//
+//  Created by Palaniya World on 28/08/14.
+//  Copyright (c) 2014 Palaniya. All rights reserved.
+//
+
+#import "SplashScreen.h"
+
+@interface SplashScreen ()
+
+@end
+
+@implementation SplashScreen
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
