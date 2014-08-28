84b7a44b6ad32d5d516125dff949235460d3edd8
Palaniya World <palaniya@Palaniyas-iMac.local>
2014-08-28 18:42:58 +0530

    Initial Commit

---
+++ BF3 States/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BF3 States
+//
+//  Created by Palaniya World on 28/08/14.
+//  Copyright (c) 2014 Palaniya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ BF3 StatesTests/BF3_StatesTests.m
@@ -0,0 +1,34 @@
+//
+//  BF3_StatesTests.m
+//  BF3 StatesTests
+//
+//  Created by Palaniya World on 28/08/14.
+//  Copyright (c) 2014 Palaniya. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BF3_StatesTests : XCTestCase
+
+@end
+
+@implementation BF3_StatesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
